05d89df0b1320ced7235c6e5fa130e831134bc58
Anis Marrouchi <anismarrouchi@hotmail.com>
2023-06-12 14:58:01 +0100

    chore: remove env variables for demo purposes as hard limit reached

--- next.config.mjs
+++ next.config.mjs
@@ -10,17 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    SUPABASE_KEY: process.env.SUPABASE_KEY,
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_BUCKET: process.env.SUPABASE_BUCKET,
-    DATABASE_URL: process.env.DATABASE_URL,
-    DIRECT_URL: process.env.DIRECT_URL,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    PINECONE_API_KEY: process.env.PINECONE_API_KEY,
-    PINECONE_ENVIRONMENT: process.env.PINECONE_ENVIRONMENT,
-    PINECONE_INDEX_NAME: process.env.PINECONE_INDEX_NAME,
-  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias.canvas = false
     config.resolve.alias.encoding = false
